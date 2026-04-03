3c0e12baa978c5e2d6deb6eac08ae949ba1c9844
madhu rathod <madhurathod2005@gmaile.com>
2026-04-02 22:08:27 -0700

    update feature

--- chat/consumers.py
+++ chat/consumers.py
@@ -22,20 +22,17 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         data = json.loads(text_data)
-        message = data["message"]
+        message = data['message']
 
-        # Broadcast message to room
         await self.channel_layer.group_send(
             self.room_group_name,
             {
-                "type": "chat_message",
-                "message": message
+                'type': 'chat_message',
+                'message': message
             }
         )
 
     async def chat_message(self, event):
-        message = event["message"]
-
         await self.send(text_data=json.dumps({
-            "message": message
+            'message': event['message']
         }))
